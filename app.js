0af8c8fcf3e09e1c2075b6d64845f62329cd6aea
Raf Swiderski <rafswid@icloud.com>
2022-02-03 16:08:46 +0000

    forwarding to callback successfully

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 const express = require('express')
 require('dotenv').config()
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
+var querystring = require('querystring');
 
 const app = express()
-const port = 3000
+const port = 8888
 
 var client_id = process.env.CLIENT_ID;
 var client_secret = process.env.CLIENT_SECRET;
 var redirect_uri = process.env.REDIRECT_URI;
 
+app.use(express.static(__dirname + '/static'))
+   .use(cors())
+   .use(cookieParser())
 
 
-var details = {
-    auth_token: 'poop'
-}
 
 app.get('/', (req, res) => {
-  res.send(details)
+  res.send('Tim Cook')
 })
 
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
 
 app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
-  var scope = 'user-read-private user-read-email';
+  res.cookie(stateKey, state);
 
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email user-top-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
@@ -32,6 +48,15 @@ app.get('/login', function(req, res) {
       redirect_uri: redirect_uri,
       state: state
     }));
+});
+
+app.get('/callback', function(req, res) {
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+
+
+  
 });
 
 
